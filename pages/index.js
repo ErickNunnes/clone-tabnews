8c9aa58ed9cff4231c7b040bf7886445f6d95760
ErickNunnes <156680798+ErickNunnes@users.noreply.github.com>
2024-01-23 13:48:10 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Lua Clara, eu te amo mais que tudo!! </h1>
+    return <h1>Lua Clara, eu te amo mais que tudo!! se você me ama da uma risadinha </h1>
 }
 
 export default Home;
