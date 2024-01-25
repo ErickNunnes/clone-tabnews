77a3029c88cff0ce7993ff7b816ee403eaffddca
ErickNunnes <156680798+ErickNunnes@users.noreply.github.com>
2024-01-25 14:24:30 +0000

    arquivo atualizado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Lua Clara, eu te amo mais que tudo!! se você me ama da uma risadinha </h1>
+  return <h1>Lua Clara, eu te amo mais que tudo!! se você me ama da uma risadinha!!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2(){
+      console.log('indentação errada')
 }
 
 export default Home;
